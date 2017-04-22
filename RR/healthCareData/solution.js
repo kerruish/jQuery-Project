85d92547d5e460e846a2952a7a0abd5b9466820c
rubabramzan96 <rubabramzan96@gmail.com>
2017-04-22 15:39:37 +0100

    HealthCareData :)

---
+++ RR/healthCareData/solution.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+        $.getJSON("Health.json", function (data) {
+            var html = "",
+            el = document.getElementById("Data");
+            el1 = document.getElementById("Data1");
+            el2 = document.getElementById("Data2");
+            el3 = document.getElementById("Data3");
+            $.each(data, function (key, val) {
+                html += "<tr><td>" + val["Statistic"] + "</td><td>"+  val["Current Health Care Expenditure (Euro Million)"]+ "</td><td>"+  val["Current Health Care Expenditure (%)"] + "</td></tr>";
+            });
+            el.innerHTML = html;
+            el1.innerHTML = html;
+            el2.innerHTML = html;
+            el3.innerHTML = html;
+            $('table.highchart').highchartTable();
+        });
+
+});
+
+
+
+
