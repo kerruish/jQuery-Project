ec911c96e5b9c25690df7d086916c12f3d695dca
BRYAN <bryan@kerruish.ie>
2017-04-22 11:53:31 +0100

    Add shell script to get the CSO data and convert it to JSON. - USE:  bash getData.sh
    Signed-off-by: BRYAN <bryan@kerruish.ie>

---
+++ our-data-site/getData.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# - retrieve the CSO JSON-stat dataset request
+# - converts to JSON saving the output to public/bin
+
+echo -en "\ec"
+echo -e "*************************************************"
+echo -e "Please enter the requested CSO dataset (e.g. EP001): "
+echo -e "*************************************************"
+echo -e ""
+read requestDataset
+echo -e ""
+echo -e "*************************************************"
+
+curl http://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/$requestDataset -o $requestDataset.jsonstat
+
+jsonstat2arrobj $requestDataset.jsonstat public/bin/$requestDataset.json
+
+rm $requestDataset.jsonstat
+
+echo -e ""
+echo -e "*************************************************"
+echo -e "That should be that!"
+echo -e "*************************************************"
